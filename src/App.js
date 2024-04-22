2681c46dd1bdc49fd84266085d4f361f6e2e3dbb
emmafguarnieri <95932570+emmafguarnieri@users.noreply.github.com>
2024-04-22 17:15:30 -0400

    changing import statement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Page from "./Components/Page";
-import Amplify from 'aws-amplify';
+import {Amplify} from 'aws-amplify';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
