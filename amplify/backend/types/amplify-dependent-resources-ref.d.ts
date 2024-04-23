552b7fba3b647f5990ff2ffeb33a276b066c3457
emmafguarnieri <95932570+emmafguarnieri@users.noreply.github.com>
2024-04-22 23:10:23 -0400

    removed api and other function

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,18 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "getAPI": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
-  "function": {
-    "newApiGetReqeust": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
